3d189a89335e7321638d11d7c39d62abf294fb5f
CoderBose <priyankabose22812@gmail.com>
2024-03-11 16:13:23 -0400

    Updating the QuizViewController

--- Trivia/QuizViewController.swift
+++ Trivia/QuizViewController.swift
@@ -8,22 +8,90 @@
 import UIKit
 
 class QuizViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet var answerButtons: [UIButton]!
+    @IBOutlet weak var questionCardLabel: UILabel!
+    
+    var currentQuestionIndex = 0
+    var correctAnswersCount = 0
 
+    var questions: [Question] = [
+        Question(text: "What is the capital of France?", answers: ["Paris", "London", "Berlin", "Madrid"], correctAnswer: "Paris"),
+        Question(text: "What is 2+2?", answers: ["4", "3", "22", "17"], correctAnswer: "4"),
+        // Add more questions here...
+    ]
+    var currentQuestion: Question?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        loadNewQuestion()
     }
     
+    @IBAction func answerButtonTapped(_ sender: UIButton) {
+        guard let title = sender.currentTitle?.trimmingCharacters(in: .whitespacesAndNewlines),
+              let question = currentQuestion else {
+            print("Something went wrong with unwrapping the button title or current question.")
+            return
+        }
 
-    /*
-    // MARK: - Navigation
+        if question.isCorrect(answer: title) {
+            correctAnswersCount += 1
+            print("Correct!")
+        } else {
+            print("Wrong!")
+        }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // Move to the next question regardless of whether the answer was correct
+        currentQuestionIndex += 1
+        loadNewQuestion()
     }
-    */
+
+    
+    func loadNewQuestion() {
+        DispatchQueue.main.async {
+            guard self.currentQuestionIndex < self.questions.count else {
+                self.questionLabel.text = "Quiz Finished"
+                self.questionCardLabel.text = "Your result: \(self.correctAnswersCount)/\(self.questions.count) correct"
+                    
+                self.answerButtons.forEach { $0.isEnabled = false }
+                return
+            }
+
+            let question = self.questions[self.currentQuestionIndex]
+            self.currentQuestion = question
+            
+            self.questionLabel.text = "Question \(self.currentQuestionIndex + 1)/\(self.questions.count)"
+            self.questionCardLabel.text = question.text
+            
+            for (index, button) in self.answerButtons.enumerated() {
+                button.isHidden = index >= question.answers.count
+                if !button.isHidden {
+                    button.setTitle(question.answers[index], for: .normal)
+                    button.isEnabled = true
+                }
+            }
+        }
+    }
+
+
 
 }
+
+
+struct Question {
+    let text: String
+    var answers: [String]
+    let correctAnswer: String
+        
+    init(text: String, answers: [String], correctAnswer: String) {
+        self.text = text
+        self.correctAnswer = correctAnswer
+        self.answers = answers.shuffled() // This shuffles the answers
+    }
+        
+    func isCorrect(answer: String) -> Bool {
+        return answer == correctAnswer
+    }
+}
+
